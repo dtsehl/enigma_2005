94d678d0efac75fb478c6039a27639f33850007e
Dan Sehl <dan.sehl@gmail.com>
2020-06-06 06:21:31 -0600

    Write test to assign A thru D keys for offset, and associated method

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,2 +1,25 @@
+require 'date'
+
 class Offset
+
+  attr_reader :date, :numbers, :a_key, :b_key, :c_key, :d_key
+
+  def initialize(date = Date.today.strftime("%d%m%y"))
+    @date = date
+    @numbers = "placeholder"
+    find_offset
+  end
+
+  def find_offset
+    squared = @date.to_i*@date.to_i
+    @numbers = squared.to_s[-4..-1]
+  end
+
+  def assign
+    @a_key = @numbers[0].to_s
+    @b_key = @numbers[1].to_s
+    @c_key = @numbers[2].to_s
+    @d_key = @numbers[3].to_s
+  end
+
 end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -7,4 +7,28 @@ class OffsetTest < Minitest::Test
     offset = Offset.new
   end
 
+  def test_it_can_have_a_date_set
+    offset = Offset.new("103030")
+    assert_equal "103030", offset.date
+  end
+
+  def test_its_date_defaults_to_today_with_no_arg
+    offset = Offset.new
+    assert_equal Date.today.strftime("%d%m%y"), offset.date
+  end
+
+  def test_it_can_find_four_digit_offset
+    offset = Offset.new("103030")
+    assert_equal "0900", offset.find_offset
+  end
+
+  def test_it_can_assign_and_have_A_through_D_keys
+    offset = Offset.new("103030")
+    offset.assign
+    assert_equal "0", offset.a_key
+    assert_equal "9", offset.b_key
+    assert_equal "0", offset.c_key
+    assert_equal "0", offset.d_key
+  end
+
 end
