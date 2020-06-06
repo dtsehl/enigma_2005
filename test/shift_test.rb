8607502202c10027c87709c33d9c40cc11905e01
Dan Sehl <dan.sehl@gmail.com>
2020-06-06 08:11:34 -0600

    Write passing #combine test and associated method for Shift

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -10,15 +10,21 @@ class ShiftTest < Minitest::Test
     assert_instance_of Shift, shift
   end
 
-  def test_it_can_sum_key_and_offset
+  def test_it_can_combine_key_and_offset
     shift = Shift.new
     key = Key.new
-    offset = Offset.new
+    key.instance_variable_set("@numbers", [0, 1, 2, 3, 4])
+    key.assign_key
+    offset = Offset.new("103030")
+    offset.find_offset
+    offset.assign_offset
+
     shift.combine(key, offset)
-    assert_equal 3, shift.a_shift
-    assert_equal 2, shift.b_shift
-    assert_equal 4, shift.c_shift
-    assert_equal 5, shift.d_shift
+
+    assert_equal 1, shift.a_shift
+    assert_equal 21, shift.b_shift
+    assert_equal 23, shift.c_shift
+    assert_equal 34, shift.d_shift
   end
 
 end
