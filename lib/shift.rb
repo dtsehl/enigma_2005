159c52e8c818fa1ae2dbb3185b4a385ebe2c51c2
Dan Sehl <dan.sehl@gmail.com>
2020-06-08 16:46:20 -0600

    Refactor shift methods to include generating keys&offsets, updated test

--- lib/shift.rb
+++ lib/shift.rb
@@ -3,17 +3,88 @@ require './lib/offset'
 
 class Shift
 
-  attr_reader :a_shift, :b_shift, :c_shift, :d_shift, :numbers
+  attr_reader :final_shift, :keys, :offsets, :date
 
-  def initialize
-    @numbers = []
+  def initialize(date, keys = nil)
+    @final_shift = []
+    @keys = keys
+    @offsets = offsets
+    combine(keys, date)
   end
 
-  def combine(key, offset)
-    @numbers << (key.a_key.to_i + offset.a_offset.to_i)
-    @numbers << (key.b_key.to_i + offset.b_offset.to_i)
-    @numbers << (key.c_key.to_i + offset.c_offset.to_i)
-    @numbers << (key.d_key.to_i + offset.d_offset.to_i)
+  def combine(keys, date)
+    if keys.nil?
+      generate
+    else
+      keys_to_array(keys)
+    end
+    @offsets = create_offset(date)
+    generate_temp_shifts(keys)
+    @final_shift.map.with_index do |shift, shift_index|
+      @final_shift[shift_index] += offsets[shift_index].to_i
+    end
+  end
+
+  def create_offset(date)
+    last_four(square(date))
+  end
+
+  def square(date)
+    date.to_i * date.to_i
+  end
+
+  def last_four(date)
+    date.to_s[-4..-1]
+  end
+
+  def generate_temp_shifts(keys)
+    @keys.each_cons(2) do |pair|
+      final_shift << pair.join.to_i
+    end
+  end
+
+  def keys_to_array(keys)
+    @keys = keys.chars.map { |key| key.to_i }
+  end
+
+  # def assign_key(given_key = nil)
+  #   if given_key.nil?
+  #     @a_key = @numbers[0].to_s + @numbers[1].to_s
+  #     @b_key = @numbers[1].to_s + @numbers[2].to_s
+  #     @c_key = @numbers[2].to_s + @numbers[3].to_s
+  #     @d_key = @numbers[3].to_s + @numbers[4].to_s
+  #   else
+  #     keys = given_key.chars.map {|key| key.to_i }
+  #     @a_key = keys[0].to_s + keys[1].to_s
+  #     @b_key = keys[1].to_s + keys[2].to_s
+  #     @c_key = keys[2].to_s + keys[3].to_s
+  #     @d_key = keys[3].to_s + keys[4].to_s
+  #   end
+  # end
+  #
+  # def assign_offset
+  #   @a_offset = @numbers[0].to_s
+  #   @b_offset = @numbers[1].to_s
+  #   @c_offset = @numbers[2].to_s
+  #   @d_offset = @numbers[3].to_s
+  # end
+
+  # def initialize(date = Date.today.strftime("%d%m%y"))
+  #   @date = date
+  #   @numbers = "placeholder"
+  # end
+
+  # def find_offset
+  #   squared = @date.to_i*@date.to_i
+  #   @numbers = squared.to_s[-4..-1]
+  # end
+
+  # def initialize
+  #   @numbers = []
+  # end
+
+  def generate
+    @keys = Array.new(5) { rand(1..9) }
   end
 
 end
