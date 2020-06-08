452888a84e7ae369f5479685e25c2fd23c5d1b5c
Dan Sehl <dan.sehl@gmail.com>
2020-06-08 14:11:00 -0600

    Refactor shift class to not use a..d_shift instance variables

--- lib/shift.rb
+++ lib/shift.rb
@@ -3,13 +3,17 @@ require './lib/offset'
 
 class Shift
 
-  attr_reader :a_shift, :b_shift, :c_shift, :d_shift
+  attr_reader :a_shift, :b_shift, :c_shift, :d_shift, :numbers
+
+  def initialize
+    @numbers = []
+  end
 
   def combine(key, offset)
-    @a_shift = (key.a_key.to_i + offset.a_offset.to_i)
-    @b_shift = (key.b_key.to_i + offset.b_offset.to_i)
-    @c_shift = (key.c_key.to_i + offset.c_offset.to_i)
-    @d_shift = (key.d_key.to_i + offset.d_offset.to_i)
+    @numbers << (key.a_key.to_i + offset.a_offset.to_i)
+    @numbers << (key.b_key.to_i + offset.b_offset.to_i)
+    @numbers << (key.c_key.to_i + offset.c_offset.to_i)
+    @numbers << (key.d_key.to_i + offset.d_offset.to_i)
   end
 
 end
