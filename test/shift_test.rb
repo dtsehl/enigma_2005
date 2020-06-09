1d162d78f7394cd87be0a9c46fc9a35fac2edf3c
Dan Sehl <dan.sehl@gmail.com>
2020-06-08 18:29:55 -0600

    Write additional shift_tests to test individual methods

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -8,6 +8,26 @@ class ShiftTest < Minitest::Test
     assert_instance_of Shift, shift
   end
 
+  def test_it_can_generate_keys
+    shift = Shift.new("040895")
+    assert_equal 5, shift.keys.count
+  end
+
+  def test_it_can_create_offsets
+    shift = Shift.new("040895", "02715")
+    assert_equal "1025", shift.create_offset("040895")
+  end
+
+  def test_it_can_square_a_date
+    shift = Shift.new("040895", "02715")
+    assert_equal 1672401025, shift.square("040895")
+  end
+
+  def test_it_can_get_last_4_digits_of_date
+    shift = Shift.new("040895", "02715")
+    assert_equal "0895", shift.last_four("040895")
+  end
+
   def test_it_can_make_final_shift
     shift = Shift.new("040895", "02715")
 
