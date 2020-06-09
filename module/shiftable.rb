d93febe2ba3f8c37ce6d3cd3fdaad6bf8287ba4a
Dan Sehl <dan.sehl@gmail.com>
2020-06-08 18:01:51 -0600

    Refactor shiftable for better test coverage

--- module/shiftable.rb
+++ module/shiftable.rb
@@ -6,8 +6,8 @@ module Shiftable
       if !@letters_array.include?(letter)
         accum << letter
       else
-        accum << @letters_array[(@letters_array.index(letter) + shift.numbers.first) % 27]
-        shift.numbers.rotate!(1)
+        accum << @letters_array[(@letters_array.index(letter) + shift.final_shift.first) % 27]
+        shift.final_shift.rotate!(1)
       end
     end
     accum.join
@@ -19,8 +19,8 @@ module Shiftable
       if !@letters_array.include?(letter)
         accum << letter
       else
-        accum << @letters_array[(@letters_array.index(letter) - shift.numbers.first) % 27]
-        shift.numbers.rotate!(1)
+        accum << @letters_array[(@letters_array.index(letter) - shift.final_shift.first) % 27]
+        shift.final_shift.rotate!(1)
       end
     end
     accum.join
